0f8440852b8f2170df74d2039dae29c699b14c3e
Dredgen Niall <theoriginalbap@gmail.com>
2021-09-19 13:40:41 -0500

    added prompt to take user input

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+
+
+
+//this captures the player's robot's name input
+var playerName = window.prompt("What is your robot's name?");
+
+//This reads back the robot's given name
+window.alert("Your robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight(){
+    window.alert("The fight has begun!");
+}
+
+fight();
+console.log(playerName + " the robot!");
